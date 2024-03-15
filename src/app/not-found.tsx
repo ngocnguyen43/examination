a45bd12210351a3bb3f38a4e6aa0402ce2a6d499
Nguyen Minh Ngoc <minhngocnguyen.403@gmail.com>
2024-03-15 23:06:59 +0700

    end :tada:

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,9 @@
+import "./globals.css"
 export default function NotFound() {
     return (
         <html>
             <body className='text-center'>
-                <h1 className='mt-10 font-semibold'>Something went wrong!</h1>
+                <h1 className='mt-10 font-semibold font-sans text-3xl'>Something went wrong!</h1>
             </body>
         </html>
     );
